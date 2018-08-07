efc2a20d256bc00b4564dbee92ff32e58ac12dfa
Sergey <ser12563208@yandex.ru>
2018-08-07 17:31:21 +0400

    icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,16 @@
 var root = document.querySelector(':root');
 var rootStyles = getComputedStyle(root);
-var mainColor = rootStyles.getPropertyValue('--main-color');
+var mainColor = rootStyles.getPropertyValue('--header-menu-display');
 
 console.log(mainColor); // '#ffeead'
+console.log(rootStyles); // '#ffeead'
+
+function toggleMenu() {
+  var showHeaderMenu = rootStyles.getPropertyValue('--header-menu-display');
+  console.log(showHeaderMenu);
+  if (showHeaderMenu =='none'){
+    root.style.setProperty('--header-menu-display','flex');
+  }else{
+    root.style.setProperty('--header-menu-display','none');
+  }
+}
